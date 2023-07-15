89e658261dfc7af6e1d5b9b11bdfeeb9168dacef
AshokVashist <ashok.vashist@gmail.com>
2023-07-15 18:52:50 +0530

    Adding a new [sum] function to the code

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print('Adding all the data processing code to fraud_detection')
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return a
+
+print('The sum of two numbers is (not correct) -> ', sum(a,b))
